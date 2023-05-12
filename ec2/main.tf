bcddee7845ea676e5fcf8601e8513ef956a5774e
shanmukh <shanmukhatadisetti@gmail.com>
2023-05-12 13:33:55 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "Centos-8-DevOPs-Practice"
+  name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["430774481266"]
 }
 
