3c356384cc5db7f640aa452e1110301785368c03
shanmukh <shanmukhatadisetti@gmail.com>
2023-05-12 12:51:33 +0530

    updating roboshop-infra

---
+++ ec2/main.tf
@@ -0,0 +1,69 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOPs-Practice"
+  owners           = ["430774481266"]
+}
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "var.component"
+  }
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.ec2.private_ip
+      user     = "centos"
+      password = "DevOps321"
+    }
+
+    inline = [
+    "git clone https://github.com/shanmukhatadisetti/roboshop-shell.git",
+    "cd roboshop-shell",
+    "sudo bash ${var.component}.sh"
+    ]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}.${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = all
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}.${var.env}-sg"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z06091892F87AU9X9DS8J"
+  name    = "dev.autonagar.in"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+variable "component" {}
+variable "instance_type" {}
+variable "env" {
+  default="dev"
+}
