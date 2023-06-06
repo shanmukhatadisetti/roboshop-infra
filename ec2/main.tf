ee13c2b3c186e76c33ebe9389ffcb3d21ca9168e
shanmukh <shanmukhatadisetti@gmail.com>
2023-06-06 22:00:21 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2" {
   iam_instance_profile   = "${var.env}-${var.component}-role"
    tags = {
     Name = var.component
-    Monitor = var.monitor ? "yes" : "no"
+    monitor = var.monitor ? "yes" : "no"
   }
 }
 
