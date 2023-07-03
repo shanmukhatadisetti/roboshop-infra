cb58416e3e473a37b60c00cdf521c1aa415a41da
shanmukh <shanmukhatadisetti@gmail.com>
2023-07-03 18:11:59 +0530

    updating roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -21,7 +21,7 @@ resource "null_resource" "provisioner" {
     }
 
     inline = [
-    "ansible-pull -i localhost, -U https://github.com/shanmukhatadisetti/roboshop-ansible.git roboshop.yml -e role_name=${var.component} -e env=${var.env}"
+    "${var.component} -e env=${var.env}"
     ]
   }
 }
