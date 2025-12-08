21db6ed0b226904f5892cc81d677a018ff5b3aaa
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2025-12-08 16:47:36 +0000

    vpc-module-development

--- 10-securitygroup/data.tf
+++ 10-securitygroup/data.tf
@@ -1,3 +1,7 @@
 data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project}/${var.env}/vpc_id"
 }
+
+data "aws_ssm_parameter" "securitygroup_id" {
+  name = "/${var.project}/${var.env}/securitygroup_id"
+}
