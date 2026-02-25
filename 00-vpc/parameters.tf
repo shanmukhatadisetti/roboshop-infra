ef44a81c5d23a39abc499840345b6618c297e2c6
shanmukha sai teja tadisetti <shanmukhatadisetti@gmail.com>
2026-02-25 14:28:35 +0000

    infra

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,21 @@ resource "aws_ssm_parameter" "roboshop_vpc_id" {
   type  = "String"
   value = module.frontend.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_id" {
+  name  = "/${var.project}/${var.env}/public_subnet_id"
+  type  = "StringList"
+  value = join(",", module.frontend.public_subnet_id)
+}
+
+resource "aws_ssm_parameter" "private_subnet_id" {
+  name  = "/${var.project}/${var.env}/private_subnet_id"
+  type  = "StringList"
+  value = join(",", module.frontend.private_subnet_id)
+}
+
+resource "aws_ssm_parameter" "db_subnet_id" {
+  name  = "/${var.project}/${var.env}/db_subnet_id"
+  type  = "StringList"
+  value = join(",", module.frontend.db_subnet_id)
+}
